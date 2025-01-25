9bb9cb46c05b2939bd47bdced54922d24de5abd6
Pedro Lucas <pedrolucasps88@outlook.com>
2025-01-25 15:43:44 -0300

    ProjetoBase_Console

---
+++ ProGymManager/Modelos/Alunos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ProGymManager.Modelos;
+
+internal class Alunos
+{
+    public string nome { get; set; }
+    public string cpf { get; set; }
+    private string numeroMatricula { get; set; }
+    public int plano { get; set; }
+    public bool ativo { get; set; }
+
+    public Alunos(string nome, string cpf)
+    {
+        this.nome = nome;
+        this.cpf = cpf;
+        numeroMatricula = Guid.NewGuid().ToString("N").Substring(0, 8);
+        ativo = true;
+        plano = 1;
+    }
+
+    public void MostrarFicha()
+    {
+        Console.WriteLine("------------------------------------");
+        Console.WriteLine($"Ficha do Aluno:{nome}");
+        Console.WriteLine($"Matricula:{numeroMatricula}");
+        Console.WriteLine(plano == 1 ? "Plano: Mensal" : "Plano: Anual");
+        Console.WriteLine(ativo ? "Aluno Ativo" : "Aluno Inativo");
+        Console.WriteLine("------------------------------------");
+    }
+}
---
+++ ProGymManager/Program.cs
@@ -0,0 +1,91 @@
+﻿
+using ProGymManager.Modelos;
+using System.ComponentModel.Design;
+Dictionary<string, Alunos> Alunos = new();
+Alunos alunos = new Alunos("João", "12345678900") { ativo = false};
+Alunos alunos1 = new Alunos("Maria", "12345678901") { plano = 2 };
+Alunos.Add(alunos.nome, alunos);
+Alunos.Add(alunos1.nome, alunos1);
+
+
+
+void menu()
+{
+    Console.Clear();
+    Console.WriteLine("WELCOME TO");
+    Console.WriteLine(@"
+
+██████╗░██████╗░░█████╗░░██████╗░██╗░░░██╗███╗░░░███╗███╗░░░███╗░█████╗░███╗░░██╗░█████╗░░██████╗░███████╗██████╗░
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░╚██╗░██╔╝████╗░████║████╗░████║██╔══██╗████╗░██║██╔══██╗██╔════╝░██╔════╝██╔══██╗
+██████╔╝██████╔╝██║░░██║██║░░██╗░░╚████╔╝░██╔████╔██║██╔████╔██║███████║██╔██╗██║███████║██║░░██╗░█████╗░░██████╔╝
+██╔═══╝░██╔══██╗██║░░██║██║░░╚██╗░░╚██╔╝░░██║╚██╔╝██║██║╚██╔╝██║██╔══██║██║╚████║██╔══██║██║░░╚██╗██╔══╝░░██╔══██╗
+██║░░░░░██║░░██║╚█████╔╝╚██████╔╝░░░██║░░░██║░╚═╝░██║██║░╚═╝░██║██║░░██║██║░╚███║██║░░██║╚██████╔╝███████╗██║░░██║
+╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░╚═════╝░░░░╚═╝░░░╚═╝░░░░░╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝░╚═════╝░╚══════╝╚═╝░░╚═╝
+    ");
+    Console.WriteLine("Escolha uma Opção:");
+
+    Console.WriteLine("1 - Cadastrar Alunos");
+    Console.WriteLine("2 - Ver Alunos");
+    Console.WriteLine("3 - Criar treino para alunos");
+    Console.WriteLine("4 - Aluno ver treino");
+    Console.WriteLine("5 - Sair");
+
+    int opcao= int.Parse(Console.ReadLine()!);
+    switch (opcao)
+    {
+        case 1:
+            CadastrarAlunos();
+            break;
+        case 2:
+            VerAlunos();
+            break;
+        case 3:
+            Console.WriteLine("TchauTchau");
+            Console.ReadLine();
+            break;
+        default:
+            Console.WriteLine("TchauTchau");
+            Console.ReadLine();
+            break;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+void CadastrarAlunos()
+{
+    Console.Clear();
+    Console.WriteLine("Cadastro de Alunos");
+    Console.WriteLine("Digite o nome do aluno:");
+    string nome = Console.ReadLine();
+    Console.WriteLine("Digite o CPF do aluno:");
+    string cpf = Console.ReadLine();
+    Alunos aluno = new Alunos(nome, cpf);
+    Alunos.Add(aluno.nome, aluno);   
+    Console.WriteLine($"O aluno {aluno.nome} foi Cadastrado com sucesso!");
+    menu();
+}
+
+void VerAlunos()
+{
+
+    Console.Clear();
+    Console.WriteLine("Alunos:");
+    foreach(var alunos in Alunos)
+    {
+        alunos.Value.MostrarFicha();
+    }
+    Console.ReadLine();
+    menu();
+}
+
+
+
+menu();
