bd82b7e0ecea6051a92782185b3d673312d2326a
Pedro Lucas <pedrolucasps88@outlook.com>
2025-03-26 11:34:53 -0300

    Feito deploy do banco e API no AZURE

--- ProGymManager.API/Program.cs
+++ ProGymManager.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using ProGymManager.API.EndPoints;
 using ProGymManager.Dados;
 using ProGymManager.Modelos;
@@ -5,8 +7,16 @@ using ProGymManager.Modelos.Modelos;
 using System.Text.Json.Serialization;
 
 
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<ProGymManagerContext>();
+builder.Services.AddDbContext<ProGymManagerContext>((options) =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:ProGymManagerDB"]);
+    var connectionString = builder.Configuration.GetConnectionString("ProGymManagerDB");
+    Console.WriteLine($"String de conexão: {connectionString}");
+});
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+Console.WriteLine($"Ambiente atual: {environment}");
 builder.Services.AddTransient<DAL<Funcionarios>>();
 builder.Services.AddTransient<DAL<Personais>>();
 builder.Services.AddTransient<DAL<Alunos>>();
@@ -30,6 +40,28 @@ app.AddEndPointsTreino();
 app.AddEndpointsSolicitacoes();
 app.AddEndPointsExercicios();
 app.AddEndPointTreinoExercicio();
+
+//using (var context = new ProGymManagerContext())
+//{
+//    try
+//    {
+//        var databaseName = context.Database.GetDbConnection().Database;
+//        Console.WriteLine($"Conectado ao banco de dados: {databaseName}");
+
+//        var tables = context.Model.GetEntityTypes()
+//            .Select(e => e.GetTableName())
+//            .ToList();
+//        Console.WriteLine("Tabelas mapeadas pelo Entity Framework:");
+//        foreach (var table in tables)
+//        {
+//            Console.WriteLine(table);
+//        }
+//    }
+//    catch (Exception ex)
+//    {
+//        Console.WriteLine($"Erro ao conectar ao banco de dados: {ex.Message}");
+//    }
+//}
 
 
 app.UseSwagger();
--- ProGymManager.Dados/ProGymManagerContext.cs
+++ ProGymManager.Dados/ProGymManagerContext.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProGymManager.Modelos;
 using ProGymManager.Modelos.Modelos;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
+using System;
 
 namespace ProGymManager.Dados
 {
     public class ProGymManagerContext : DbContext
     {
+        //private string connectionString = "Server=tcp:progymmanagerserver.database.windows.net,1433;Initial Catalog=ProGymManagerDB;Persist Security Info=False;User ID=pedro;Password=Progymmanager88;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private string connectionString = "Server=(localdb)\\mssqllocaldb;Database=ProGymManagerDB;Trusted_Connection=True;";
         public DbSet<Alunos> Alunos { get; set; }
         public DbSet<Exercicios> Exercicios { get; set; }
         public DbSet<Funcionarios> Funcionarios { get; set; }
@@ -18,15 +24,16 @@ namespace ProGymManager.Dados
         { }
 
         // Construtor sem parâmetros para suportar o Design Time (em migrations, por exemplo)
-        public ProGymManagerContext()
-        { }
+        //public ProGymManagerContext()
+        //{ }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ProGymManagerDB;Integrated Security=True;");
-            }
+            //if (!optionsBuilder.IsConfigured)
+            //{
+            //    return;
+            //}
+            //optionsBuilder.UseSqlServer(connectionString);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
